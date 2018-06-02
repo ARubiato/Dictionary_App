b34c8dd6b44c5863fb2b81762e7a7d0ac9cb9636
ARubiato <alessanderrubiato@yahoo.com>
2018-06-03 01:00:51 +0800

    added input forms

--- create_dictionary.php
+++ create_dictionary.php
@@ -24,10 +24,16 @@
 				</nav>
 
 				<div class = "Create_intro">
-					<h3>My Creation</h3> <Button class = "Button1" value = "submit" style ="border-radius: 5px;">+ CREATE</Button>
+						<form action = "create_dictionary_form.php">
+					<h3>My Creation</h3> <Button class = "Button1" value = "submit" style ="border-radius: 5px; margin: 1em auto; width: 60%;">+ CREATE</Button>
 				</div>
+			</form>
 				<div class = "Create_body">
-					
+					<div class = "Dictionaries_created">
+						  <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
+					</div>
 				</div>	
 
 			</div>
